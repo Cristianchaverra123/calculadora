4cdb36ed66e88ec75223b17c2211d746b4cc8385
Cristian <cristian.chaverra@sumimedical.com>
2026-09-23 14:42:54 -0500

    Agregar potencia a la calculadora

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ $num2 = 5;
 echo "Suma: " . ($num1 + $num2) . "\n";
 echo "Resta: " . ($num1 - $num2) . "\n";
 echo "Multiplicación: " . ($num1 * $num2) . "\n";
-echo "División: " . ($num1 / $num2); 
+echo "División: " . ($num1 / $num2) . "\n";
+echo "Potencia: " . ($num1 ** $num2);
