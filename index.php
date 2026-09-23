d3e844d8a4ce053142f17af37c29ec1c51a918fa
Cristian <cristian.chaverra@sumimedical.com>
2026-09-23 12:30:14 -0500

    Agregar operaciones a la calculadora

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
 $num1 = 10;
 $num2 = 5;
 
-echo $num1 + $num2;
+echo "Suma: " . ($num1 + $num2) . "\n";
+echo "Resta: " . ($num1 - $num2) . "\n";
+echo "Multiplicación: " . ($num1 * $num2) . "\n";
+echo "División: " . ($num1 / $num2); 
